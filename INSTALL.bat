8dba3ba3378739308f48be98fd699391f9f1cbe7
lolyinseo <lolyinseo@gmail.com>
2024-08-06 21:22:48 +0300

    Update INSTALL.bat

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,11 +1,9 @@
 @echo off
 echo Creating virtual environment...
-python -m venv venv
+python -m hamsterbot venv
 echo Activating virtual environment...
-call venv\Scripts\activate
+call hamsterbot\Scripts\activate
 echo Installing dependencies...
 pip install -r requirements.txt
-echo Copying .env-example to .env...
-copy .env-example .env
 echo Please edit the .env file to add your API_ID and API_HASH.
 pause
